997b03272ef97210fadd337c38e2b9f9821856fc
Arjun Dhanjal <hello@arjundhanjal.com>
2015-03-16 19:18:31 -0400

    ps_get-mappeddrives
    Get-MappedDrives allows you to pull the mapped drives for the currently
    logged-on user for any computer with PowerShell installed.

---
+++ Get-MappedDrives.ps1
@@ -0,0 +1,29 @@
+<#
+Get-MappedDrives v2
+Developed by Arjun Dhanjal
+March 2015
+
+Usage: .\Get-MappedDrives.ps1 -ComputerName <Workstation Name>
+
+CHANGELOG:
+  v2: Script rewritten from batch file to PowerShell script.
+  v1: Original script written for Scotiabank as a *.bat.
+#>
+
+[CmdletBinding()]
+	PARAM(
+		[Parameter(Mandatory=$true, HelpMessage=" ")]
+		[string]$ComputerName,
+		[Parameter(Mandatory=$true, HelpMessage=" ")]
+		[string]$SamAccountName
+	)
+
+Write-Host "Drives currently mapped on ${ComputerName} for user ${SamAccountName}:"
+$Mapped = Get-WmiObject -Class "Win32_MappedLogicalDisk" -Namespace "root\CIMV2" -ComputerName $ComputerName | Select DeviceID,ProviderName
+ForEach ($drive in $mapped) {
+  Write-Host -NoNewLine $drive.DeviceID "" -ForegroundColor Cyan
+  Write-Host $drive.ProviderName -ForegroundColor Green
+}
+
+Write-Host "Press any key to continue..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")

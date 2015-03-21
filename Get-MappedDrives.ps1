6d4b0fff02832eaa411d094dc93948809418b871
Arjun Dhanjal <hello@arjundhanjal.com>
2015-03-21 03:06:26 -0400

    added license information to header

--- Get-MappedDrives.ps1
+++ Get-MappedDrives.ps1
@@ -1,7 +1,21 @@
 <#
-Get-MappedDrives v2.1
-Developed by Arjun Dhanjal
-March 2015
+Get-MappedDrives.ps1 (v2.1)
+Copyright (C) 2015  Arjun Dhanjal
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contact information available at ArjunDhanjal.com
 
 Usage: .\Get-MappedDrives.ps1 -ComputerName <Workstation Name> -SamAccountName <Target User>
 
